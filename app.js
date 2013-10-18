a676ebfa613a303682809639d81cf741de55610d
Cheah Chu Yeow <chuyeow@gmail.com>
2013-10-18 22:41:07 +0800

    Mount all methods on /v2 so we can segregate it from the existing API.

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var koa = require('koa')
   , koaLogger = require('koa-logger')
-  , config = require('./lib/config')
-  , router = require('koa-router');
+  , mount = require('koa-mount')
+  , Router = require('koa-router')
+  , config = require('./lib/config');
 
 var app = koa();
 app.use(koaLogger());
-app.use(router(app));
 
-app.get('/', function *() {
-  this.body = 'OHAI';
+var apiVersion2 = new Router();
+
+apiVersion2.get('/anime/:id', function *(id) {
+  var anime = yield function *() { return id };
+  this.body = anime;
 });
+
+app.use(mount('/v2', apiVersion2.middleware()));
 
 app.listen(config.get('port'), function() {
   console.log('API server listening on port ' + config.get('port'));
